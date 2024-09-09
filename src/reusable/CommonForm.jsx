3a4562873c3e0fd2494fca004548070a9316169c
lokesh-esds <lokesh.wani@esds.co.in>
2024-09-09 14:58:11 +0530

    Contacts Page Form Made Dynamic

--- src/reusable/CommonForm.jsx
+++ src/reusable/CommonForm.jsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import Form from 'react-bootstrap/Form';
-import Col from 'react-bootstrap/Col';
-import { Row } from 'react-bootstrap';
-import Button from 'react-bootstrap/Button';
-import Select from 'react-select';
-import Flatpickr from 'react-flatpickr';
+import React from "react";
+import Form from "react-bootstrap/Form";
+import Col from "react-bootstrap/Col";
+import { Row } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import Select from "react-select";
+import Flatpickr from "react-flatpickr";
 
-const CommonForm = ({ viewOnly }) => {
+const CommonForm = ({ viewOnly, addFormProps }) => {
   const pricingOptions = [
-    { value: '1', label: 'Pricing Option 1' },
-    { value: '2', label: 'Pricing Option 2' },
-    { value: '3', label: 'Pricing Option 3' },
+    { value: "1", label: "Pricing Option 1" },
+    { value: "2", label: "Pricing Option 2" },
+    { value: "3", label: "Pricing Option 3" },
   ];
   return (
     <>
@@ -18,8 +18,13 @@ const CommonForm = ({ viewOnly }) => {
         <Row>
           <Col xl="12">
             <Form.Group className="form-group mb-2" controlId="formGroupText">
-              <Form.Label>Select Product</Form.Label>
-              <Form.Select aria-label="Default select example" disabled={viewOnly}>
+              <Form.Label>
+                {addFormProps ? addFormProps.title : "Add pricing Master"}
+              </Form.Label>
+              <Form.Select
+                aria-label="Default select example"
+                disabled={viewOnly}
+              >
                 <option>Select Product</option>
                 <option value="1">One</option>
                 <option value="2">Two</option>
@@ -27,37 +32,110 @@ const CommonForm = ({ viewOnly }) => {
               </Form.Select>
             </Form.Group>
           </Col>
+          {addFormProps && (
+            <>
+            <Col xl="6">
+              <Form.Group className="form-group mb-2" controlId="formGroupText">
+                <Form.Label>
+                  {addFormProps?addFormProps.fisrtName : ""}
+                </Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="First Name"
+                  disabled={viewOnly}
+                  />
+              </Form.Group>
+            </Col>
+
+            <Col xl="6">
+            <Form.Group className="form-group mb-2" controlId="formGroupText">
+              <Form.Label>
+                {addFormProps?addFormProps.middleName : ""}
+              </Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Middle Name"
+                disabled={viewOnly}
+                />
+            </Form.Group>
+          </Col>
           <Col xl="6">
             <Form.Group className="form-group mb-2" controlId="formGroupText">
-              <Form.Label>Pricing Level</Form.Label>
-              <Form.Select aria-label="Default select example" disabled={viewOnly}>
-                <option>Pricing</option>
+              <Form.Label>
+                {addFormProps?addFormProps.lastName : ""}
+              </Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="last Name"
+                disabled={viewOnly}
+                />
+            </Form.Group>
+          </Col>
+        
+          <Col xl="6">
+              <Form.Group className="form-group mb-2" controlId="formGroupText">
+                <Form.Label>
+                  {addFormProps?addFormProps.dob : ""}
+                </Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="DOB"
+                  disabled={viewOnly}
+                  />
+              </Form.Group>
+            </Col>
+                </>
+          )}
+         
+          <Col xl="6">
+            <Form.Group className="form-group mb-2" controlId="formGroupText">
+              <Form.Label>{addFormProps?addFormProps.companyName:""}</Form.Label>
+              <Form.Select
+                aria-label="Company Name"
+                disabled={viewOnly}
+              >
+                {
+                  addFormProps.companyName?
+                  <>
+                <option>Select Option</option>
+                <option value="1">Company One</option>
+                <option value="2">Company Two </option>
+                <option value="3">Company Three</option>
+                  </>
+                :
+                <>
+                <option>Select Option</option>
                 <option value="1">One</option>
                 <option value="2">Two</option>
                 <option value="3">Three</option>
+                </>
+}
               </Form.Select>
             </Form.Group>
           </Col>
           <Col xl="6">
             <Form.Group className="form-group mb-2" controlId="formGroupText">
               <Form.Label>Pricing List</Form.Label>
-              <Select
-                isMulti
-                options={pricingOptions}
-                isDisabled={viewOnly}
-              />
+              <Select isMulti options={pricingOptions} isDisabled={viewOnly} />
             </Form.Group>
           </Col>
           <Col xl="6">
             <Form.Group className="form-group mb-2" controlId="formGroupText">
               <Form.Label>Purchase Cost</Form.Label>
-              <Form.Control type="text" placeholder="Purchase Cost (One Time)" disabled={viewOnly} />
+              <Form.Control
+                type="text"
+                placeholder="Purchase Cost (One Time)"
+                disabled={viewOnly}
+              />
             </Form.Group>
           </Col>
           <Col xl="6">
             <Form.Group className="form-group mb-2" controlId="formGroupText">
               <Form.Label>Purchase Currency</Form.Label>
-              <Form.Select aria-label="Default select example" disabled={viewOnly}>
+              <Form.Select
+                aria-label="Default select example"
+                disabled={viewOnly}
+              >
                 <option>Purchase Currency</option>
                 <option value="1">One</option>
                 <option value="2">Two</option>
@@ -68,13 +146,20 @@ const CommonForm = ({ viewOnly }) => {
           <Col xl="6">
             <Form.Group className="form-group mb-2" controlId="formGroupText">
               <Form.Label>Purchase Cost</Form.Label>
-              <Form.Control type="text" placeholder="Purchase Cost (Recurring)" disabled={viewOnly} />
+              <Form.Control
+                type="text"
+                placeholder="Purchase Cost (Recurring)"
+                disabled={viewOnly}
+              />
             </Form.Group>
           </Col>
           <Col xl="6">
             <Form.Group className="form-group mb-2" controlId="formGroupText">
               <Form.Label>Recurring Currency</Form.Label>
-              <Form.Select aria-label="Default select example" disabled={viewOnly}>
+              <Form.Select
+                aria-label="Default select example"
+                disabled={viewOnly}
+              >
                 <option>Recurring Currency</option>
                 <option value="1">One</option>
                 <option value="2">Two</option>
@@ -85,13 +170,20 @@ const CommonForm = ({ viewOnly }) => {
           <Col xl="6">
             <Form.Group className="form-group mb-2" controlId="formGroupText">
               <Form.Label>Selling Price</Form.Label>
-              <Form.Control type="text" placeholder="Selling Price (One Time)" disabled={viewOnly} />
+              <Form.Control
+                type="text"
+                placeholder="Selling Price (One Time)"
+                disabled={viewOnly}
+              />
             </Form.Group>
           </Col>
           <Col xl="6">
             <Form.Group className="form-group mb-2" controlId="formGroupText">
               <Form.Label>Selling Currency</Form.Label>
-              <Form.Select aria-label="Default select example" disabled={viewOnly}>
+              <Form.Select
+                aria-label="Default select example"
+                disabled={viewOnly}
+              >
                 <option>Selling Currency</option>
                 <option value="1">One</option>
                 <option value="2">Two</option>
@@ -102,13 +194,20 @@ const CommonForm = ({ viewOnly }) => {
           <Col xl="6">
             <Form.Group className="form-group mb-2" controlId="formGroupText">
               <Form.Label>Selling Price</Form.Label>
-              <Form.Control type="text" placeholder="Selling Price (Recurring)" disabled={viewOnly} />
+              <Form.Control
+                type="text"
+                placeholder="Selling Price (Recurring)"
+                disabled={viewOnly}
+              />
             </Form.Group>
           </Col>
           <Col xl="6">
             <Form.Group className="form-group mb-2" controlId="formGroupText">
               <Form.Label>Recurring Selling Currency</Form.Label>
-              <Form.Select aria-label="Default select example" disabled={viewOnly}>
+              <Form.Select
+                aria-label="Default select example"
+                disabled={viewOnly}
+              >
                 <option>Selling Currency</option>
                 <option value="1">One</option>
                 <option value="2">Two</option>
@@ -130,34 +229,51 @@ const CommonForm = ({ viewOnly }) => {
           <Col xl="6">
             <Form.Group className="form-group mb-2" controlId="formGroupText">
               <Form.Label>Default Upgrade Forecasted One Time Price</Form.Label>
-              <Form.Control type="text" placeholder="One Time Price" disabled={viewOnly} />
+              <Form.Control
+                type="text"
+                placeholder="One Time Price"
+                disabled={viewOnly}
+              />
             </Form.Group>
           </Col>
           <Col xl="6">
             <Form.Group className="form-group mb-2" controlId="formGroupText">
               <Form.Label>Forecasted Recurring Price</Form.Label>
-              <Form.Control type="text" placeholder="Forecasted Recurring Price" disabled={viewOnly} />
+              <Form.Control
+                type="text"
+                placeholder="Forecasted Recurring Price"
+                disabled={viewOnly}
+              />
             </Form.Group>
           </Col>
           <Col xl="6">
             <Form.Group className="form-group mb-2" controlId="formGroupText">
               <Form.Label>Forecasted Tenure Months</Form.Label>
-              <Form.Control type="text" placeholder="Forecasted Tenure Months" disabled={viewOnly} />
+              <Form.Control
+                type="text"
+                placeholder="Forecasted Tenure Months"
+                disabled={viewOnly}
+              />
             </Form.Group>
           </Col>
         </Row>
         <Row>
-          <Col xl="12" className='CrmFormFooter'>
-            <Button type="submit" disabled={viewOnly}>Submit</Button>
-            <Button type="reset" className='btn btn-default' disabled={viewOnly}>Reset</Button>
+          <Col xl="12" className="CrmFormFooter">
+            <Button type="submit" disabled={viewOnly}>
+              Submit
+            </Button>
+            <Button
+              type="reset"
+              className="btn btn-default"
+              disabled={viewOnly}
+            >
+              Reset
+            </Button>
           </Col>
         </Row>
       </Form>
     </>
   );
-}
+};
 
 export default CommonForm;
-
-
-
